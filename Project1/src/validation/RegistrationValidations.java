34bf7733338212f6ca19bdb78a5fc9b041b68ab1
Violeta-star-art <violetateova@gmail.com>
2021-03-02 19:44:16 +0100

    adding new hello

--- Project1/src/validation/RegistrationValidations.java
+++ Project1/src/validation/RegistrationValidations.java
@@ -5,6 +5,6 @@ public class RegistrationValidations {
 
         System.out.println("SMile and Run");
         System.out.println("Hello Sun");
-
+      System.out.println("Hello Everyone");
     }
 }
